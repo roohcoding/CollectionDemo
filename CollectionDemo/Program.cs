a948e7ef3473b7c832941994778d6407040bb9a2
Rupali Natewad <rupalirnatewad@gmail.com>
2022-05-25 10:54:13 +0530

    Add project files.

---
+++ CollectionDemo/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CollectionDemo;
+
+Console.WriteLine("Hello, World!");
+CollectionSample.doListDemo();
+CollectionSample.doStackDemo();
+CollectionSample.doDictionaryDemo();
+CollectionSample.doQueueDemo();
+CollectionSample.doSetDemo();
+Console.ReadKey();
